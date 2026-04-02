ba076d0c7c32e2591098145c6ac666dbd9742ab2
unknown <245070@student.uek.krakow.pl>
2026-04-02 23:11:13 +0200

    deleted core and static, refactored config

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin', admin.site.urls),
+
+    path('api/auth', include('accounts.auth_urls')),
+    path('api/accounts', include('accounts.urls')),
+    path('api/posts', include('posts.urls')),
+]
